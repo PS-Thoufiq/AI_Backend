ca63444c9e4752b8866b3ea912d5dd1b3b5e0a10
PS-Thoufiq <thoufiq.ps@zeero.us>
2025-08-22 11:39:37 +0530

    added port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 3002;
 
 app.use(cors());
 app.use(express.json());
